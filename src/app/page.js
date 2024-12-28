b94e4c8a211eb4a175caa02faf6dd2abf5579b4f
SeongJun Ha <agopwns@gmail.com>
2024-12-28 22:35:22 +0900

    바텀 네비게이션 바

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,28 @@
-import Image from "next/image";
+import { Home as HomeIcon, MessageCircle, User } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button className="btn btn-outline">Click me</button>
-      <button className="btn">Click me</button>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary">Click me</button>
-      <button className="btn btn-accent">Click me</button>
-      <button className="btn btn-ghost">Click me</button>
-      <button className="btn btn-link">Click me</button>
+      {/* 하단 네비게이션 바 */}
+      <nav className="fixed bottom-0 left-0 right-0 bg-black text-white p-4">
+        <div className="flex justify-around items-center max-w-screen-xl mx-auto">
+          <Link href="/" className="flex flex-col items-center">
+            <HomeIcon className="h-6 w-6" />
+            <span className="text-xs mt-1">홈</span>
+          </Link>
+
+          <Link href="/chat" className="flex flex-col items-center">
+            <MessageCircle className="h-6 w-6" />
+            <span className="text-xs mt-1">채팅</span>
+          </Link>
+
+          <Link href="/mypage" className="flex flex-col items-center">
+            <User className="h-6 w-6" />
+            <span className="text-xs mt-1">나의 땅콩</span>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 }
